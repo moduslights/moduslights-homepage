ca43a3427b35e96178fc35884af158664c4e5de6
Babatunde Alaraje <balaraje@kacytunde-2.attlocal.net>
2020-09-23 02:30:04 -0500

    update new code

---
+++ build/precache-manifest.9182ddf98970ebd54569c3d77291d0c0.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "590d1da9a45aea597a6872f08804c7a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70089a0ce1bcd8db4ba7",
+    "url": "/static/css/2.ac850543.chunk.css"
+  },
+  {
+    "revision": "0068ec30e4236142d6a0",
+    "url": "/static/css/3.859f00eb.chunk.css"
+  },
+  {
+    "revision": "6ee80213c8c071126278",
+    "url": "/static/css/main.ac08de4d.chunk.css"
+  },
+  {
+    "revision": "70089a0ce1bcd8db4ba7",
+    "url": "/static/js/2.5f0e7d49.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/static/js/2.5f0e7d49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0068ec30e4236142d6a0",
+    "url": "/static/js/3.82961908.chunk.js"
+  },
+  {
+    "revision": "275fe79abee3b697f1673c8bd9c58856",
+    "url": "/static/js/3.82961908.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "922251ea4b75cd9dc0bd",
+    "url": "/static/js/4.12318fd4.chunk.js"
+  },
+  {
+    "revision": "6ee80213c8c071126278",
+    "url": "/static/js/main.8d210328.chunk.js"
+  },
+  {
+    "revision": "be292487cf12c2c4b8d5",
+    "url": "/static/js/runtime-main.86a80b77.js"
+  },
+  {
+    "revision": "2cf6052a47b2e0566e599d159ba927d3",
+    "url": "/static/media/ModusLightsLogoOnlyTransparentBg.2cf6052a.jpg"
+  },
+  {
+    "revision": "73160335b87ffd37f185a3b9e3b96a93",
+    "url": "/static/media/bg1.73160335.jpg"
+  },
+  {
+    "revision": "2e19c22e788d3d5dd2e35588f69b4c5b",
+    "url": "/static/media/bg2.2e19c22e.jpg"
+  },
+  {
+    "revision": "e5ac82a258a1c1a4d62b7bacae7195c8",
+    "url": "/static/media/bg3.e5ac82a2.jpg"
+  },
+  {
+    "revision": "cd2a55b682f6476b2db8b71a95c94b0a",
+    "url": "/static/media/bg4.cd2a55b6.jpg"
+  },
+  {
+    "revision": "4321a15d0a8a36a89f7886292ad2de80",
+    "url": "/static/media/bg5.4321a15d.jpg"
+  },
+  {
+    "revision": "84bb445a0fbf95a580f214866eb71512",
+    "url": "/static/media/devComputer.84bb445a.jpg"
+  },
+  {
+    "revision": "14137f89247c1ab0eb29e8b75ef6e9d6",
+    "url": "/static/media/google-icon.14137f89.svg"
+  },
+  {
+    "revision": "d56c859f8a5e503c78287d51b208e723",
+    "url": "/static/media/hero-1.d56c859f.jpg"
+  },
+  {
+    "revision": "71e9487c209545f06ff9d46ea7d6e6a4",
+    "url": "/static/media/hero-5.71e9487c.jpg"
+  },
+  {
+    "revision": "32c5d875e80f697b301a039a9a150c9f",
+    "url": "/static/media/imageonline_co_blackandwhiteimage.32c5d875.jpg"
+  },
+  {
+    "revision": "3d5d10b881e444c6fea07b13c9e8e29d",
+    "url": "/static/media/instagram-icon.3d5d10b8.svg"
+  },
+  {
+    "revision": "f32b3f892b2ff3a1fbd1d7cead2d120b",
+    "url": "/static/media/microsoft-icon.f32b3f89.svg"
+  },
+  {
+    "revision": "674adafe5c40e6ce0937be8e23b4b589",
+    "url": "/static/media/particles-2.674adafe.svg"
+  },
+  {
+    "revision": "11a8f47e01fe816b8ef48fd9fd3bb810",
+    "url": "/static/media/react.11a8f47e.svg"
+  },
+  {
+    "revision": "82a07222e832194941c57503c9281fcd",
+    "url": "/static/media/spotify-icon.82a07222.svg"
+  },
+  {
+    "revision": "9b2cf53cde11a230f866a62cb959ac9f",
+    "url": "/static/media/stock-6.9b2cf53c.jpg"
+  },
+  {
+    "revision": "3cae6a0e87792c7d11cf6687f20d34a4",
+    "url": "/static/media/stock-7.3cae6a0e.jpg"
+  }
+]);
