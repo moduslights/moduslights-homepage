e3429915681e854ace7590d40eb5e4a54e0b1dd4
Babatunde Alaraje <balaraje@kacytunde-2.attlocal.net>
2020-09-23 01:07:13 -0500

    add build

---
+++ build/precache-manifest.646c866eb3e3fb29ca7975cf0d5ce296.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ea34d9beff32bf11ceb18fbb35cffec",
+    "url": "/bamburgh-react-admin-dashboard-reactstrap-pro/index.html"
+  },
+  {
+    "revision": "922678daa7540966f306",
+    "url": "/bamburgh-react-admin-dashboard-reactstrap-pro/static/css/2.ac850543.chunk.css"
+  },
+  {
+    "revision": "fb07c1161f8ce75b202d",
+    "url": "/bamburgh-react-admin-dashboard-reactstrap-pro/static/css/3.859f00eb.chunk.css"
+  },
+  {
+    "revision": "91bc1cde28eb4863d7f1",
+    "url": "/bamburgh-react-admin-dashboard-reactstrap-pro/static/css/main.df4af9a1.chunk.css"
+  },
+  {
+    "revision": "922678daa7540966f306",
+    "url": "/bamburgh-react-admin-dashboard-reactstrap-pro/static/js/2.ce0b8106.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/bamburgh-react-admin-dashboard-reactstrap-pro/static/js/2.ce0b8106.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb07c1161f8ce75b202d",
+    "url": "/bamburgh-react-admin-dashboard-reactstrap-pro/static/js/3.caeedb5e.chunk.js"
+  },
+  {
+    "revision": "275fe79abee3b697f1673c8bd9c58856",
+    "url": "/bamburgh-react-admin-dashboard-reactstrap-pro/static/js/3.caeedb5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d8efd733aaf435fc6de",
+    "url": "/bamburgh-react-admin-dashboard-reactstrap-pro/static/js/4.1fce79c7.chunk.js"
+  },
+  {
+    "revision": "91bc1cde28eb4863d7f1",
+    "url": "/bamburgh-react-admin-dashboard-reactstrap-pro/static/js/main.59894905.chunk.js"
+  },
+  {
+    "revision": "557550291de5c2cf8145",
+    "url": "/bamburgh-react-admin-dashboard-reactstrap-pro/static/js/runtime-main.c7530115.js"
+  },
+  {
+    "revision": "2cf6052a47b2e0566e599d159ba927d3",
+    "url": "/bamburgh-react-admin-dashboard-reactstrap-pro/static/media/ModusLightsLogoOnlyTransparentBg.2cf6052a.jpg"
+  },
+  {
+    "revision": "73160335b87ffd37f185a3b9e3b96a93",
+    "url": "/bamburgh-react-admin-dashboard-reactstrap-pro/static/media/bg1.73160335.jpg"
+  },
+  {
+    "revision": "2e19c22e788d3d5dd2e35588f69b4c5b",
+    "url": "/bamburgh-react-admin-dashboard-reactstrap-pro/static/media/bg2.2e19c22e.jpg"
+  },
+  {
+    "revision": "e5ac82a258a1c1a4d62b7bacae7195c8",
+    "url": "/bamburgh-react-admin-dashboard-reactstrap-pro/static/media/bg3.e5ac82a2.jpg"
+  },
+  {
+    "revision": "cd2a55b682f6476b2db8b71a95c94b0a",
+    "url": "/bamburgh-react-admin-dashboard-reactstrap-pro/static/media/bg4.cd2a55b6.jpg"
+  },
+  {
+    "revision": "4321a15d0a8a36a89f7886292ad2de80",
+    "url": "/bamburgh-react-admin-dashboard-reactstrap-pro/static/media/bg5.4321a15d.jpg"
+  },
+  {
+    "revision": "84bb445a0fbf95a580f214866eb71512",
+    "url": "/bamburgh-react-admin-dashboard-reactstrap-pro/static/media/devComputer.84bb445a.jpg"
+  },
+  {
+    "revision": "14137f89247c1ab0eb29e8b75ef6e9d6",
+    "url": "/bamburgh-react-admin-dashboard-reactstrap-pro/static/media/google-icon.14137f89.svg"
+  },
+  {
+    "revision": "d56c859f8a5e503c78287d51b208e723",
+    "url": "/bamburgh-react-admin-dashboard-reactstrap-pro/static/media/hero-1.d56c859f.jpg"
+  },
+  {
+    "revision": "71e9487c209545f06ff9d46ea7d6e6a4",
+    "url": "/bamburgh-react-admin-dashboard-reactstrap-pro/static/media/hero-5.71e9487c.jpg"
+  },
+  {
+    "revision": "32c5d875e80f697b301a039a9a150c9f",
+    "url": "/bamburgh-react-admin-dashboard-reactstrap-pro/static/media/imageonline_co_blackandwhiteimage.32c5d875.jpg"
+  },
+  {
+    "revision": "3d5d10b881e444c6fea07b13c9e8e29d",
+    "url": "/bamburgh-react-admin-dashboard-reactstrap-pro/static/media/instagram-icon.3d5d10b8.svg"
+  },
+  {
+    "revision": "f32b3f892b2ff3a1fbd1d7cead2d120b",
+    "url": "/bamburgh-react-admin-dashboard-reactstrap-pro/static/media/microsoft-icon.f32b3f89.svg"
+  },
+  {
+    "revision": "674adafe5c40e6ce0937be8e23b4b589",
+    "url": "/bamburgh-react-admin-dashboard-reactstrap-pro/static/media/particles-2.674adafe.svg"
+  },
+  {
+    "revision": "11a8f47e01fe816b8ef48fd9fd3bb810",
+    "url": "/bamburgh-react-admin-dashboard-reactstrap-pro/static/media/react.11a8f47e.svg"
+  },
+  {
+    "revision": "82a07222e832194941c57503c9281fcd",
+    "url": "/bamburgh-react-admin-dashboard-reactstrap-pro/static/media/spotify-icon.82a07222.svg"
+  },
+  {
+    "revision": "9b2cf53cde11a230f866a62cb959ac9f",
+    "url": "/bamburgh-react-admin-dashboard-reactstrap-pro/static/media/stock-6.9b2cf53c.jpg"
+  },
+  {
+    "revision": "3cae6a0e87792c7d11cf6687f20d34a4",
+    "url": "/bamburgh-react-admin-dashboard-reactstrap-pro/static/media/stock-7.3cae6a0e.jpg"
+  }
+]);
